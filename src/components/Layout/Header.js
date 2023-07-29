7aff93e794b274ca9442fd37552cf830dd7ebb88
umang718 <umangvirat@gmail.com>
2023-07-29 12:25:04 +0530

    Completed food order app for frontend only.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import {Fragment} from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton onClick = {props.onShowCart}/>
+            <button>
+                Cart
+            </button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src = {mealsImage} alt = "a table full of delicious food"/>
+        </div>
+    </Fragment>
+    );
+};
+
+export default Header;
